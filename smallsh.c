0ea1689e7e96cee906e5fc58d130e392bfe03d5e
Aeijan Bajracharya <bajracae@os1.engr.oregonstate.edu>
2019-11-17 16:22:52 -0800

    working on redirect

--- smallsh.c
+++ smallsh.c
@@ -11,13 +11,17 @@ int MAX_NUM_OF_ARGS = 512;
 bool ifValidArgNum(char * input);
 bool signExists(char * input);
 char * expandUserInput(char * input);
-char ** commandArr(char ** array, char * input, int * numStr);
-void printArr(char ** array, int numStr);
+char ** commandArr(char ** array, char * input, int * num_of_strings);
+void printArr(char ** array, int num_of_strings);
 void cdCommand(char ** array); 
 char ** createArr();
 bool isComment(char ** array);
-bool ambExist(char ** array, int numStr);
+bool ambExist(char ** array, int num_of_strings);
 bool notBulitIn(char ** array);
+void redirectCommand(char ** array, int num_of_strings);
+void nonBuiltCommand(char ** user_array, int num_of_strings);
+bool hasRedirection(char ** user_array, int num_of_strings);
+void removeRedirection(char ** user_array, int num_of_strings);
 
 int main() {
     int num_of_strings = 0;
@@ -59,8 +63,9 @@ int main() {
             // run the status function
         }
         else {
-            // execvp(user_array[0], user_array); // Runs commands that are not bulit in
-            nonBuiltCommand(user_array);
+            nonBuiltCommand(user_array, num_of_strings);
+            // redirectCommand(user_array, num_of_strings);
+            // nonBuiltCommand(user_array);
             continue;
         }
 
@@ -76,6 +81,88 @@ int main() {
     return 0;
 }
 
+void nonBuiltCommand(char ** user_array, int num_of_strings) {
+    pid_t spawnpid = -5;
+    int childExitedMethod = -5;
+    
+    int inputFile = 0;
+    int outputFile = 0;
+    int result = 0;
+    int i;
+
+    spawnpid = fork();
+    switch (spawnpid) {
+        case -1: // if error in forking()
+            perror("ERROR WHILE FORKING");
+            exit(1);
+            break;
+        case 0: // child process stuff runs here 
+            if(hasRedirection(user_array, num_of_strings) == true) {
+                for(i = num_of_strings; i > 0; i--) {
+                    // redirect to stdin
+                    if(strcmp(user_array[i-2], "<") == 0) {
+                        inputFile = open(user_array[i-1], O_RDONLY);
+                        if(inputFile == -1) {
+                            printf("Input file could not be opened for reading.\n");
+                            // fflush();
+                            exit(1); // How to not exit the shell??
+                        }
+                        result = dup2(inputFile, 0);
+                        if(result == -1) {
+                            printf("(error with stdin)\n");
+                        }
+                        
+                    }
+                
+                    // redirect to stdout
+                    else if(strcmp(user_array[i-2], ">") == 0) {
+                        outputFile = open(user_array[i-1], O_WRONLY | O_CREAT | O_TRUNC, 0644); // These will go into dup
+                        if(outputFile == -1) {
+                            printf("Input file could not be opened for writing.\n");
+                            // fflush();
+                            exit(1); // How to not exit the shell??
+                        }
+                        result = dup2(outputFile, 1);
+                        if(result == -1) {
+                            printf("(error with stdout)\n");
+                        }
+                        
+                    }
+                
+                    else {
+                        continue;
+                    }
+
+                }
+            }
+            removeRedirection(user_array, num_of_strings);
+            execvp(user_array[0], user_array);
+            break;
+        default: // parent process
+            waitpid(spawnpid, &childExitedMethod, 0); // Block the parent until the child process with specified PID terminates
+            break;
+    }
+}
+
+void removeRedirection(char ** user_array, int num_of_strings) {
+    int i;
+    int count = 0;
+
+        for(i = 0; i < num_of_strings; i++) {
+            printf("this: %s\n", user_array[i]);
+            if((strcmp(user_array[i], ">") == 0) || (strcmp(user_array[i], "<") == 0)) {
+                printf("I am here3\n");
+                count = i;
+                break;
+            }
+        }
+        if(count != 0) {
+            for(count; count < num_of_strings; count++) {
+                user_array[count] = NULL;
+            }
+        }
+}
+
 bool ifValidArgNum(char * input) {
     int count = 1;
     int i;
@@ -86,6 +173,16 @@ bool ifValidArgNum(char * input) {
     }
     if(count <= MAX_NUM_OF_ARGS) {
         return true;
+    }
+    return false;
+}
+
+bool signExists(char * input) {
+    int i;
+    for(i = 0; i < strlen(input); i++) {
+        if((input[i] == '$') && (input[i+1] == '$')) {
+            return true;
+        }
     }
     return false;
 }
@@ -120,23 +217,6 @@ char ** createArr() {
     return array;
 }
 
-char ** commandArr(char ** array, char * input, int * numStr) {
-    int i, j;
-    j = 0; 
-    for(i = 0; i < strlen(input) + 1; i++) {
-        if(input[i] == ' ' || input[i] == '\0') {
-            array[*numStr][j] = '\0';
-            j = 0;
-            (*numStr)++;
-        }
-        else {
-            array[*numStr][j] = input[i];
-            j++;    
-        }
-    }
-    return array;
-}
-
 int countWords(char * user_input) {
     int num_of_strings = 0;
     int i;
@@ -148,9 +228,9 @@ int countWords(char * user_input) {
     return num_of_strings;
 }
 
-void printArr(char ** array, int numStr) {
+void printArr(char ** array, int num_of_strings) {
     int i;
-    for(i = 0; i < numStr; i++) {
+    for(i = 0; i < num_of_strings; i++) {
         printf("%s\n", array[i]);
     }
 }
@@ -185,28 +265,50 @@ void statusCommand(char ** array) {
     // Print out exit status or the terminating signal of the last foreground process
 }
 
-bool ambExist(char ** array, int numStr) {
-    if(strcmp(array[numStr-1], "&") == 0){
+bool ambExist(char ** array, int num_of_strings) {
+    if(strcmp(array[num_of_strings-1], "&") == 0){
         return true;
     }
     return false;
 }
 
-bool signExists(char * input) {
+
+void redirectCommand(char ** user_array, int num_of_strings) {
+    int inputFile = 0;
+    int outputFile = 0;
+    int result = 0;
     int i;
-    for(i = 0; i < strlen(input); i++) {
-        if((input[i] == '$') && (input[i+1] == '$')) {
-            return true;
+    
+    for(i = num_of_strings; i > 0; i--) {
+        // redirect to stdin
+        if(strcmp(user_array[i-2], "<") == 0) {
+            inputFile = open(user_array[i-1], O_RDONLY);
+            if(inputFile == -1) {
+                printf("Input file could not be opened for reading.\n");
+                // fflush();
+                exit(1); // How to not exit the shell??
+            }
+            printf("error<\n");
+            result = dup2(inputFile, 0);
+        }
+    
+        // redirect to stdout
+        else if(strcmp(user_array[i-2], ">") == 0) {
+            outputFile = open(user_array[i-1], O_WRONLY | O_CREAT | O_TRUNC, 0644); // These will go into dup
+            if(outputFile == -1) {
+                printf("Input file could not be opened for writing.\n");
+                // fflush();
+                exit(1); // How to not exit the shell??
+            }
+                printf("error>\n");
+                result = dup2(outputFile, 1);
+        }
+    
+        else {
+            continue;
         }
     }
-    return false;
-}
-
-void redirectCommand(char ** array, int numStr) {
-    int inputFile;
-    int outputFile;
-    int result;
-    int i;
+    execvp(user_array[0], user_array);
     
     // using > or < to redirection
     // dup2()
@@ -215,71 +317,54 @@ void redirectCommand(char ** array, int numStr) {
     // background process redirect to /dev/null
     // open the file to /dev/null, dup2(), stdin = 0;
 
-    if(ambExist(array, numStr) == true) {
-        for(i = numStr-1; i > 0; i--) {
-            if(strcmp(array[i-2], "<") == 0) {
-                inputFile = open(array[i-1], O_RDONLY);
-                if(inputFile < 0) {
-                    printf("Input file could not be opened for reading.\n");
-                    // fflush();
-                    exit(1); // How to not exit the shell??
-                }
-                else {
-                    result = dup2(inputFile, 0);
-                }
-            }
-                    
-            // redirect to stdout
-            if(strcmp(array[i-2], ">") == 0) {
-                outputFile = open(array[i-1], O_WRONLY | O_CREAT | O_TRUNC); // These will go into dup
-                if(outputFile < 0) {
-                    printf("Input file could not be opened for writing.\n");
-                    // fflush();
-                    exit(1); // How to not exit the shell??
-                }
-                else {
-                    result = dup2(outputFile, 1);
-                }
-            }
-        }
-        execvp(array[0], array);
-    }
-    else {
-        for(i = numStr; i > 0; i--) {
-            // redirect to stdin
-            if(strcmp(array[i-2], "<") == 0) {
-                inputFile = open(array[i-1], O_RDONLY);
-                if(inputFile < 0) {
-                    printf("Input file could not be opened for reading.\n");
-                    // fflush();
-                    exit(1); // How to not exit the shell??
-                }
-                else {
-                    result = dup2(inputFile, 0);
-                }
-            }
-            
-            // redirect to stdout
-            if(strcmp(array[i-2], ">") == 0) {
-                outputFile = open(array[i-1], O_WRONLY | O_CREAT | O_TRUNC); // These will go into dup
-                if(outputFile < 0) {
-                    printf("Input file could not be opened for writing.\n");
-                    // fflush();
-                    exit(1); // How to not exit the shell??
-                }
-                else {
-                    result = dup2(outputFile, 1);
-                }
-            }
-        }
-        execvp(array[0], array);
-    }
+    // if(ambExist(array, num_of_strings) == true) {
+    //     for(i = num_of_strings-1; i > 0; i--) {
+    //         if(strcmp(array[i-2], "<") == 0) {
+    //             inputFile = open(array[i-1], O_RDONLY);
+    //             if(inputFile < 0) {
+    //                 printf("Input file could not be opened for reading.\n");
+    //                 fflush();
+    //                 exit(1); // How to not exit the shell??
+    //             }
+    //             else {
+    //                 result = dup2(inputFile, 0);
+    //             }
+    //         }
+    // 
+    //         // redirect to stdout
+    //         if(strcmp(array[i-2], ">") == 0) {
+    //             outputFile = open(array[i-1], O_WRONLY | O_CREAT | O_TRUNC); // These will go into dup
+    //             if(outputFile < 0) {
+    //                 printf("Input file could not be opened for writing.\n");
+    //                 fflush();
+    //                 exit(1); // How to not exit the shell??
+    //             }
+    //             else {
+    //                 result = dup2(outputFile, 1);
+    //             }
+    //         }
+    //     }
+    //     execvp(array[0], array);
+    // }
+    // else {
+    
+    // }
 }
 
 
 bool notBulitIn(char ** array) {
     if((strcmp(array[0], "cd") != 0) && (strcmp(array[0], "exit") != 0) && (strcmp(array[0], "status") != 0)) {
         return true;    
+    }
+    return false;
+}
+
+bool hasRedirection(char ** user_array, int num_of_strings) {
+    int i;
+    for(i = 0; i < num_of_strings; i++) {
+        if((strcmp(user_array[i], "<") == 0) || (strcmp(user_array[i], ">") == 0)){
+            return true;
+        }
     }
     return false;
 }
