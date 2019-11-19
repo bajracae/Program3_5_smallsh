214fcc54347702d39f6b513ff0c70c4349a582d5
Aeijan Bajracharya <bajracae@os1.engr.oregonstate.edu>
2019-11-18 17:09:34 -0800

    status command works

--- smallsh.c
+++ smallsh.c
@@ -4,6 +4,7 @@
 #include <stdbool.h>
 #include <dirent.h>
 #include <fcntl.h>
+#include <signal.h>
 
 int MAX_LENGTH_OF_STR = 2048;
 int MAX_NUM_OF_ARGS = 512;
@@ -14,21 +15,32 @@ char * expandUserInput(char * input);
 char ** commandArr(char ** array, char * input, int * num_of_strings);
 void printArr(char ** array, int num_of_strings);
 void cdCommand(char ** array); 
+void exitCommand();
 char ** createArr();
 bool isComment(char ** array);
 bool ambExist(char ** array, int num_of_strings);
-void redirectCommand(char ** array, int num_of_strings);
-void nonBuiltCommand(char ** user_array, int num_of_strings);
+void nonBuiltBackground(char ** user_array, int num_of_strings);
+void nonBuiltForeground(char ** user_array, int num_of_strings, int * childExitedMethod);
 bool hasRedirection(char ** user_array, int num_of_strings);
 void removeRedirection(char ** user_array, int num_of_strings);
+void statusCommand(int childExitedMethod);
 
 int main() {
     int num_of_strings = 0;
-    char * user_input;
-    char * expanded_user_input;
+    int childExitedMethod = -5;
+    char * user_input = NULL;
+    char * expanded_user_input = NULL;
     char ** user_array = createArr();
     int i;
     size_t input_size = MAX_LENGTH_OF_STR;
+    pid_t pid_array[1000];
+    
+    // Catch ctrl-C to terminate foreground
+    // https://stackoverflow.com/questions/1641182/how-can-i-catch-a-ctrl-c-event
+    // struct sigaction controlCHandler;
+    // sigaction(SIGINT, &controlCHandler, NULL);
+    // create a function that prints out the error
+    
     do {
         printf(": ");
         fflush(stdout);
@@ -43,7 +55,6 @@ int main() {
         }
         
         i = 1;    
-            
         // Place each string from the input into the array until NULL terminator is found
         user_array[0] = strtok(expanded_user_input, " \n");    
         while((user_array[i] = strtok(NULL, " \n"))) i++;
@@ -56,34 +67,29 @@ int main() {
             cdCommand(user_array);
         }
         else if(strcmp(user_array[0], "exit") == 0) {
-            // run the exit function
+            exitCommand();
         }
         else if(strcmp(user_array[0], "status") == 0) {
-            // run the status function
+            statusCommand(childExitedMethod);
         }
         else {
-            nonBuiltCommand(user_array, num_of_strings);
-            // redirectCommand(user_array, num_of_strings);
-            // nonBuiltCommand(user_array);
-            continue;
-        }
-
-
-        // if(notBulitIn(array) == true) {
-        //     printf("I made it here\n");
-        //     execvp(array[0], array);
-        // }
-    
+            if(ambExist(user_array, num_of_strings)) {
+                nonBuiltBackground(user_array, num_of_strings);
+            }
+            else {
+                nonBuiltForeground(user_array, num_of_strings, &childExitedMethod);
+            }
+            // continue;
+        }    
     } while(strcmp(user_input, "exit") != 0);
     
     
     return 0;
 }
 
-void nonBuiltCommand(char ** user_array, int num_of_strings) {
+void nonBuiltForeground(char ** user_array, int num_of_strings, int * childExitedMethod) {
     pid_t spawnpid = -5;
-    int childExitedMethod = -5;
-    
+    // int childExitedMethod = 0;
     int inputFile = 0;
     int outputFile = 0;
     int result = 0;
@@ -132,10 +138,105 @@ void nonBuiltCommand(char ** user_array, int num_of_strings) {
                 }
             }
             removeRedirection(user_array, num_of_strings);
+            printf("I ran here\n");
             execvp(user_array[0], user_array);
             break;
         default: // parent process
-            waitpid(spawnpid, &childExitedMethod, 0); // Block the parent until the child process with specified PID terminates
+            waitpid(spawnpid, childExitedMethod, 0); // Block the parent until the child process with specified PID terminates
+            break;
+    }
+}
+
+void nonBuiltBackground(char ** user_array, int num_of_strings) {
+    pid_t spawnpid = -5;
+    int childExitedMethod = 0;
+    int inputFile = 0;
+    int outputFile = 0;
+    int devNULLInput = 0;
+    int devNULLOutput = 0;
+    int result = 0;
+    int i;
+    
+    spawnpid = fork();
+    switch (spawnpid) {
+        case -1:
+            perror("ERROR WHILE FORKING");
+            exit(1);
+            break;
+        case 0:
+            // print out the background PID here;
+            printf("background pid is %d\n", getpid());
+            if(ambExist(user_array, num_of_strings) == true) {
+                if(hasRedirection(user_array, num_of_strings) == true) {
+                    for(i = num_of_strings-1; i >= 0; i--) {
+                        if(strcmp(user_array[i], "<") == 0) {
+                            if((strcmp(user_array[i+1], "&") == 0)) {
+                                // redirect to /dev/null
+                                devNULLInput = open("/dev/null", O_RDONLY);
+                                if(devNULLInput == -1) {
+                                    printf("Input file could not be opened for reading.\n");
+                                    fflush(stdout);
+                                    exit(1); // How to not exit the shell??
+                                }
+                                result = dup2(devNULLInput, 0);
+                                if(result == -1) {
+                                    printf("(error with stdin)\n");
+                                    fflush(stdout);
+                                }
+                            }
+                            else {
+                                inputFile = open(user_array[i+1], O_RDONLY);
+                                if(inputFile == -1) {
+                                    printf("Input file could not be opened for reading.\n");
+                                    fflush(stdout);
+                                    exit(1); // How to not exit the shell??
+                                }
+                                result = dup2(inputFile, 0);
+                                if(result == -1) {
+                                    printf("(error with stdin)\n");
+                                    fflush(stdout);
+                                }
+                            }
+                        }
+                        else if(strcmp(user_array[i], ">") == 0) {
+                            if((strcmp(user_array[i+1], "&") == 0)) {
+                                devNULLOutput = open("/dev/null", O_WRONLY | O_CREAT | O_TRUNC, 0644);
+                                if(devNULLOutput == -1) {
+                                    printf("Input file could not be opened for reading.\n");
+                                    fflush(stdout);
+                                    exit(1); // How to not exit the shell??
+                                }
+                                result = dup2(devNULLOutput, 0);
+                                if(result == -1) {
+                                    printf("(error with stdin)\n");
+                                    fflush(stdout);
+                                }
+                            }
+                            else {
+                                outputFile = open(user_array[i+1], O_WRONLY | O_CREAT | O_TRUNC, 0644);
+                                if(outputFile == -1) {
+                                    printf("Input file could not be opened for writing.\n");
+                                    fflush(stdout);
+                                    exit(1); // How to not exit the shell??
+                                }
+                                result = dup2(outputFile, 1);
+                                if(result == -1) {
+                                    printf("(error with stdout)\n");
+                                    fflush(stdout);
+                                }
+                            }
+                        }
+                        else {
+                            continue;
+                        }
+                    }
+                }
+                removeRedirection(user_array, num_of_strings);
+                execvp(user_array[0], user_array);
+            }
+            break;
+        case 1:
+            waitpid(spawnpid, &childExitedMethod, WNOHANG);
             break;
     }
 }
@@ -143,18 +244,17 @@ void nonBuiltCommand(char ** user_array, int num_of_strings) {
 void removeRedirection(char ** user_array, int num_of_strings) {
     int i;
     int count = 0;
-
-        for(i = 0; i < num_of_strings; i++) {
-            if((strcmp(user_array[i], ">") == 0) || (strcmp(user_array[i], "<") == 0)) {
-                count = i;
-                break;
-            }
+    for(i = 0; i < num_of_strings; i++) {
+        if((strcmp(user_array[i], ">") == 0) || (strcmp(user_array[i], "<") == 0)) {
+            count = i;
+            break;
         }
-        if(count != 0) {
-            for(count; count < num_of_strings; count++) {
-                user_array[count] = NULL;
-            }
+    }
+    if(count != 0) {
+        for(count; count < num_of_strings; count++) {
+            user_array[count] = NULL;
         }
+    }
 }
 
 bool ifValidArgNum(char * input) {
@@ -222,13 +322,6 @@ int countWords(char * user_input) {
     return num_of_strings;
 }
 
-void printArr(char ** array, int num_of_strings) {
-    int i;
-    for(i = 0; i < num_of_strings; i++) {
-        printf("%s\n", array[i]);
-    }
-}
-
 void cdCommand(char ** user_array) {
     char dir_path[256];
 
@@ -243,20 +336,39 @@ void cdCommand(char ** user_array) {
     fflush(stdout);
 }
 
-void exitCommand(char ** array) {
-    if(strcmp(array[1], "") == 0) {
-            // Kill everything
-            // kill child and foreground processes
-            // holder for all pid of child processes
-            // run sigkill for each child process
+void exitCommand(char ** user_array, pid_t * pid_array, int pid_array_len) {
+    // Kill everything
+    // kill child and foreground processes
+    // holder for all pid of child processes
+    // run sigkill for each child process
+
+    int i;
+    if(user_array[1] == NULL) {
+        for(i = 0; i < size; i++) {
+            if(pid_array[i] != 0) {
+                
+            }
+        }
+        exit(0);
     }
     else {
         printf("ERROR\n");
+        fflush(stdout);
     }
 }
 
-void statusCommand(char ** array) {
-    // Print out exit status or the terminating signal of the last foreground process
+void statusCommand(int childExitedMethod) {
+    int termSig = 0;
+    int exitStat = 0;
+    // LECTURE 3.1 Processes
+    if(WIFSIGNALED(childExitedMethod) != 0) {
+        termSig = WTERMSIG(childExitedMethod);
+        printf("terminated by signal %d\n", termSig);
+    }
+    else {
+        exitStat = WEXITSTATUS(childExitedMethod);
+        printf("exit value %d\n", exitStat);
+    }
 }
 
 bool ambExist(char ** array, int num_of_strings) {
@@ -264,85 +376,6 @@ bool ambExist(char ** array, int num_of_strings) {
         return true;
     }
     return false;
-}
-
-
-void redirectCommand(char ** user_array, int num_of_strings) {
-    int inputFile = 0;
-    int outputFile = 0;
-    int result = 0;
-    int i;
-    
-    for(i = num_of_strings; i > 0; i--) {
-        // redirect to stdin
-        if(strcmp(user_array[i-2], "<") == 0) {
-            inputFile = open(user_array[i-1], O_RDONLY);
-            if(inputFile == -1) {
-                printf("Input file could not be opened for reading.\n");
-                // fflush();
-                exit(1); // How to not exit the shell??
-            }
-            printf("error<\n");
-            result = dup2(inputFile, 0);
-        }
-    
-        // redirect to stdout
-        else if(strcmp(user_array[i-2], ">") == 0) {
-            outputFile = open(user_array[i-1], O_WRONLY | O_CREAT | O_TRUNC, 0644); // These will go into dup
-            if(outputFile == -1) {
-                printf("Input file could not be opened for writing.\n");
-                // fflush();
-                exit(1); // How to not exit the shell??
-            }
-                printf("error>\n");
-                result = dup2(outputFile, 1);
-        }
-    
-        else {
-            continue;
-        }
-    }
-    execvp(user_array[0], user_array);
-    
-    // using > or < to redirection
-    // dup2()
-    // cat < red needs to become cat
-    // cat < red > output also n eeds to become cat
-    // background process redirect to /dev/null
-    // open the file to /dev/null, dup2(), stdin = 0;
-
-    // if(ambExist(array, num_of_strings) == true) {
-    //     for(i = num_of_strings-1; i > 0; i--) {
-    //         if(strcmp(array[i-2], "<") == 0) {
-    //             inputFile = open(array[i-1], O_RDONLY);
-    //             if(inputFile < 0) {
-    //                 printf("Input file could not be opened for reading.\n");
-    //                 fflush();
-    //                 exit(1); // How to not exit the shell??
-    //             }
-    //             else {
-    //                 result = dup2(inputFile, 0);
-    //             }
-    //         }
-    // 
-    //         // redirect to stdout
-    //         if(strcmp(array[i-2], ">") == 0) {
-    //             outputFile = open(array[i-1], O_WRONLY | O_CREAT | O_TRUNC); // These will go into dup
-    //             if(outputFile < 0) {
-    //                 printf("Input file could not be opened for writing.\n");
-    //                 fflush();
-    //                 exit(1); // How to not exit the shell??
-    //             }
-    //             else {
-    //                 result = dup2(outputFile, 1);
-    //             }
-    //         }
-    //     }
-    //     execvp(array[0], array);
-    // }
-    // else {
-    
-    // }
 }
 
 bool hasRedirection(char ** user_array, int num_of_strings) {
@@ -354,10 +387,3 @@ bool hasRedirection(char ** user_array, int num_of_strings) {
     }
     return false;
 }
-
-// using > or < to redirection
-// dup2()
-// cat < red needs to become cat
-// cat < red > output also n eeds to become cat
-// background process redirect to /dev/null
-// open the file to /dev/null, dup2(), stdin = 0;
